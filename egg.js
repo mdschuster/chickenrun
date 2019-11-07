f677b403c032ec822fb989d1e210276064cf71e0
Micah Schuster <micahdschuster@gmail.com>
2019-11-07 08:58:36 -0500

    Add license information to each file

--- egg.js
+++ egg.js
@@ -1,8 +1,19 @@
 /**
  * Egg class for dino run clone
- * Copyright (C) 2019 by Micah Schuster
+ * Copyright (C) 2019 Micah Schuster
  * 
- * This code is licensed under the GPL
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  * 
  * The Egg class stores the data and
  * draws the graphics for the obsticals
